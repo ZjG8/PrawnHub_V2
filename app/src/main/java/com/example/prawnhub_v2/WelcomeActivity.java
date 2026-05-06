69b04553392e048744385c43d8fd36cc13fc038c
ZjG8 <sijeyvasquez@gmail.com>
2026-05-07 02:37:40 +0800

    Update WelcomeActivity.java

--- app/src/main/java/com/example/prawnhub_v2/WelcomeActivity.java
+++ app/src/main/java/com/example/prawnhub_v2/WelcomeActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+// welcome
 public class WelcomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
